7a89cf4d0105d121091e467682f6d9361f1a39fe
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2018-09-23 22:39:42 +0800

    Added Student model and migrated the project models

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    student_number = models.CharField(max_length=7)
+    last_name = models.UpperCharField()
+    first_name = models.UpperCharField()
+    year_level = (
+        ('1ST YEAR', '1ST YEAR'),
+        ('2ND YEAR', '2ND YEAR'),
+        ('3RD YEAR', '3RD YEAR'),
+        ('4TH YEAR', '4TH YEAR'),
+        ('5TH YEAR', '5TH YEAR'),
+    )
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.last_name + " " + self.first_name
